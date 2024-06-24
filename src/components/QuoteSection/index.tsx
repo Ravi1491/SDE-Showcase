36770481b665c4ebeb1de762a22918cebd4dd1e7
Ravi Singh <ravi149185@gmail.com>
2024-06-25 02:39:05 +0530

    Dr. Who Section

--- src/components/QuoteSection/index.tsx
+++ src/components/QuoteSection/index.tsx
@@ -1,10 +1,9 @@
 import Quote from "@/assets/icons/Quote";
-import React from "react";
 
 const QuoteSection = () => {
   return (
-    <div className="flex h-24 justify-center items-center">
-      <div className="relative flex w-fit p-6 border-2 border-gray">
+    <div className="flex flex-col h-32 justify-center items-center">
+      <div className="relative flex w-fit p-6 px-8 border-2 border-gray">
         <div className="absolute top-0 left-10 transform -translate-x-1/2 -translate-y-1/2 z-10">
           <div className="absolute top-2 left-3 w-10 h-10 bg-background rounded-full border-gray transform -translate-x-1/2 -translate-y-1/2 z-0"></div>
           <Quote className="relative z-10" />
@@ -15,6 +14,16 @@ const QuoteSection = () => {
         <div className="absolute bottom-0 right-10 transform translate-x-1/2 translate-y-1/2 z-10">
           <div className="absolute bottom-2 right-3 w-10 h-10 bg-background rounded-full border-gray transform translate-x-1/2 translate-y-1/2 z-0"></div>
           <Quote className="relative z-10" />
+        </div>
+        {/* <div className="w-fit absolute top-12 right-16 transform translate-x-1/2 translate-y-1/2 z-10">
+          <div className="flex border-l-2 border-r-2 border-b-2 py-4 px-5 border-gray">
+            <span className="text-md">- Dr. Who</span>
+          </div>
+        </div> */}
+      </div>
+      <div className="w-full flex items-end justify-end">
+        <div className="flex mr-52 border-l-2 border-r-2 border-b-2 py-4 px-4 border-gray">
+          <span className="flex">- Dr. Who</span>
         </div>
       </div>
     </div>
